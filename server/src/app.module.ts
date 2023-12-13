0e4b94a7aad5b39264c3ac694ea321991cfa4127
JongInWon <jonginwon1203@gmail.com>
2023-12-13 19:33:21 +0900

    feat: users, posts 뼈대 추가, docker 설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
